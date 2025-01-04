b45277f753114f5bad47d02ff29fa15cf74ae694
Nic Cheneweth <nchenewe@thoughtworks.com>
2025-01-04 17:20:36 -0600

    [nc] renaming for clarity
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- scripts/install_metrics_server.sh
+++ scripts/install_metrics_server.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 set -eo pipefail
+source bash-functions.sh
 
 cluster_name=$1
 CHART_VERSION=$(jq -er .metrics_server_chart_version environments/$cluster_name.json)
 
 echo "metrics-server chart version $CHART_VERSION"
 helm repo add metrics-server https://kubernetes-sigs.github.io/metrics-server/
+
+# perform trivy scan of chart with install configuration
+trivyScan "metrics-server/metrics-server" "metrics-server"  "$CHART_VERSION" "metrics-server-values/$cluster_name-values.yaml"
+
 helm upgrade --install metrics-server metrics-server/metrics-server \
              --version $CHART_VERSION \
              --namespace kube-system \
-             --values metrics-server/$cluster_name-values.yaml
+             --values metrics-server-values/$cluster_name-values.yaml
