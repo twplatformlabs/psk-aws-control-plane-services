aa1b10e47ecdc39a1492ec37ff822108fef3e992
Nic Cheneweth <nchenewe@thoughtworks.com>
2025-01-04 23:28:20 -0600

    [nc] add hpa testing
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- scripts/services_integration_test.sh
+++ scripts/services_integration_test.sh
@@ -1,15 +1,29 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-
+# wait a minute for the services to be ready
 sleep 60
+
 # run basic smoketest for service health
-bats test/services-integration-test.bats
+bats test/services-state-test.bats
 
-# run hpa test to confirm metrics-server health
+# run horizontalpodautoscaler test to confirm metrics-server working health
+kubectl apply -f test/hpa-test-deployment.yaml
+sleep 30
+kubectl apply -f test/hap-test-load-generator.yaml
+sleep 120
 
+replicas=$(kubectl get hpa php-apache | awk 'NR > 1 { print $7 }')
+echo "hpa reports $replicas replicas"
 
+if [[ "$replicas" > 1 ]]; then
+  echo "hpa-test replicas scaled under load."
+else
+  echo "error: hpa-test replicas not scaling under load."
+  exit 1
+fi
+kubectl delete -f test/hpa-test-deployment.yaml
+kubectl delete -f test/hap-test-load-generator.yaml
 
-# run basi
-# run hpa test to confirm metrics-server health
-
+# run test metrics output test to confirm kube-state-metrics and event-exporter health
+bats test/services_functional_test.bats
